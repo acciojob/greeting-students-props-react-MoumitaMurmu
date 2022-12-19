8f61e29b77c3e351de1a610e3a1af25b88b669b0
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2022-12-20 01:21:32 +0530

    done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,11 @@
-// write code for Welcome component here
 import React from 'react'
 
-function Welcome(props) {
-  return (
-    <div>
-        <h1>Hey {props.name}!</h1>
-        <h2>Welcome to Newton School</h2>
-    </div>
-  )
-}
+export default function child(props) {
 
-export default Welcome
+    return (
+        <div>
+            <h1>Hey {props.name}!</h1>
+            <h2>Welcome to Newton School.</h2>
+        </div>
+    )
+}
